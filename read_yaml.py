61a449193aefd44319a988ffd5690c84f5fe8301
Yubin <iamyubinkarki@gmail.com>
2022-05-08 15:04:40 +0545

    Write specific data into text file for intent and entity.

--- read_yaml.py
+++ read_yaml.py
@@ -13,15 +13,21 @@ def yamltotext(filename):
             if intent_type == "intent":
                 utterances = item["utterances"]
                 
-                with open(f"data/Text_Files/{intent_name}.txt", mode = "w+") as intent_file:
+                with open(f"data/Text_Files/Utterances/{intent_name}.txt", mode = "w+") as intent_file:
                     for i, name in enumerate(utterances):
                         # print("Index: ", i, "\tLength:", len(utterances) - 1)
                         if len(utterances) -1 == i:
                             newline = ""  # To remove new line at the end of file.
                         intent_file.write(name + newline)  # Adding data to text file with new line.
-        
+
+            elif intent_type == "entity":
+                entity_values = str(item["values"])
+                with open(f"data/Text_Files/Entity/{intent_name}.txt", mode = "w+") as entity_file:
+                    entity_file.write(entity_values) 
+                    
+
         return "Text files created successfully."
 
 
-file_to_convert = "data/test.yaml"
+file_to_convert = "data/dataset.yaml"
 print(yamltotext(file_to_convert))
